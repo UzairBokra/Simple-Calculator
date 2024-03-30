05c553a9a5dcaa23ec30861b1ef31c8565b0f089
UzairBokra <uzair.bokra@gmail.com>
2024-03-30 08:16:39 -0700

    My first commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node   // ya line important ha  
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "fistNumber"},
+    {message:"Enter your second number", type:"number",name:"secondNumber"},
+    {
+        message:"Select one of the opeators to perform operation",
+        type:"list",
+        name:"operators",
+        choices:["Addition","Subtraction","Multiplication","Division"],
+    }
+])
+if(answer.operators === "Addition") {
+    console.log(`Your answer is: ${answer.fistNumber + answer.secondNumber}`);
+}else if(answer.operators === "Subtraction") {
+    console.log(`Your answer is: ${answer.fistNumber - answer.secondNumber}`);
+}else if(answer.operators === "Multiplication") {
+    console.log(`Your answer is: ${answer.fistNumber * answer.secondNumber}`);
+}else if(answer.operators === "Division") {
+    console.log(`Your answer is: ${answer.fistNumber / answer.secondNumber}`);
+}else {
+    console.log("Please slect a valid operators");
+}
+
+
+
+
+
